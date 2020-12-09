45a04d2803eb9a23d478e9f90a8ed35ca3668af7
Doraemon123-alt <72442959+Doraemon123-alt@users.noreply.github.com>
2020-12-09 11:42:15 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,7 +1,7 @@
 class Bob {
     constructor(x, y) {
       var options = {
-          restitution:0,
+          restitution:1,
           friction:0,
           inertia:Infinity,
           frictionAir:0.0 
